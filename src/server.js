ae209d9cd7e1e9e8d1b2eb9116a59649bd70486c
lucastenani <lucas.tenani3@gmail.com>
2024-06-26 16:30:52 -0300

    Create Routes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,35 @@
 import htpp from 'node:http'
 
+const tasks = []
+
 const server = htpp.createServer((req, res) => {
-  return res.end('Hello World!')
+  const { method, url } = req
+  console.log(method)
+  console.log(url)
+
+  if (method === 'GET' && url === '/tasks') {
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(tasks))
+  } else if (method === 'POST' && url === '/tasks') {
+    tasks.push({
+      id: 1,
+      title: 'Cycling',
+      description: 'Cycling on the street',
+      completed_at: null,
+      created_at: String(new Date()),
+      updated_at: String(new Date()),
+    })
+    return res.writeHead(201).end()
+  } else if (method === 'PUT' && url === '/tasks/:id') {
+    return res.writeHead(404).end()
+  } else if (method === 'DELETE' && url === '/tasks/:id') {
+    return res.writeHead(404).end()
+  } else if (method === '/tasks/:id/complete' && url === '/tasks/:id') {
+    return res.writeHead(404).end()
+  }
+
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
