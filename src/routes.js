ef1a99254fc313597ebfea20fd8059df13f2d30a
lucastenani <lucas.tenani3@gmail.com>
2024-06-27 17:22:53 -0300

    When field is missing return error

--- src/routes.js
+++ src/routes.js
@@ -26,34 +26,50 @@ export const routes = [
     method: 'POST',
     path: buildRoutePath('/tasks'),
     handler: (req, res) => {
-      const { title, description } = req.body
+      try {
+        const { title, description } = req.body
 
-      const task = {
-        title,
-        description,
-        completed_at: null,
+        if (!title || !description) {
+          throw new Error()
+        }
+
+        const task = {
+          title,
+          description,
+          completed_at: null,
+        }
+
+        database.insert('tasks', task)
+
+        return res.writeHead(201).end()
+      } catch (error) {
+        return res.writeHead(400).end('Mandatory fields are missing')
       }
-
-      database.insert('tasks', task)
-
-      return res.writeHead(201).end()
     },
   },
   {
     method: 'PUT',
     path: buildRoutePath('/tasks/:id'),
     handler: (req, res) => {
-      const { id } = req.params
-      const { title, description } = req.body
+      try {
+        const { id } = req.params
+        const { title, description } = req.body
 
-      const task = {
-        title,
-        description,
+        if (!title || !description) {
+          throw new Error()
+        }
+
+        const task = {
+          title,
+          description,
+        }
+
+        database.update('tasks', id, task)
+
+        res.writeHead(204).end()
+      } catch (error) {
+        res.writeHead(400).end('Mandatory fields are missing')
       }
-
-      database.update('tasks', id, task)
-
-      res.writeHead(204).end()
     },
   },
   {
